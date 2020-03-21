2045fd6b20049ee212243a11a92346f8114a6367
VisioNaryy <dofeternity@gmail.com>
2020-03-21 15:04:30 +0300

    External libraries

--- DiplomaApp-SPA/src/app/_services/auth.service.ts
+++ DiplomaApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
 constructor(private http: HttpClient) {  }
 
@@ -17,12 +20,23 @@ login(model: any) {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          // save token to service
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
         }
       })
     );
 }
 
-register(model: any) {
-  return this.http.post(this.baseUrl + 'register', model);
-}
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+
+    // if token is expired, or there is no token etc , this will return false
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
 }
