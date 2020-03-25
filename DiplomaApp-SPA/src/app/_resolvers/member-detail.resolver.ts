cd9a44c61b4da7f7d9fd865ce7f42c1a4141383a
VisioNaryy <dofeternity@gmail.com>
2020-03-25 23:33:34 +0300

    Member components, route resolvers, gallery

---
+++ DiplomaApp-SPA/src/app/_resolvers/member-detail.resolver.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class MemberDetailResolver implements Resolve<User> {
+    /**
+     *
+     */
+    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
+
+    // this method gets the user service, gets the user, matches the route parameter that we
+    // are aiming to get and then catchs the error (if no errors - continue the route that we are activating)
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(route.params.id).pipe(
+            catchError(error => {
+                this.alertify.error('Problem retrieving data');
+                this.router.navigate(['/members']);
+                return of(null);
+            })
+        );
+    }
+}
